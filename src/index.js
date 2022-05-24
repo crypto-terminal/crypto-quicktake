455b83f0393afbd51647ef0d5087b01fb3a9e3de
Yumin Gui <yumindev@gmail.com>
2022-05-23 22:36:08 -0700

    add libraries

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import { createRoot } from 'react-dom/client';
 import { App } from "./App";
+import { ChakraProvider } from "@chakra-ui/react";
 
 const container = document.getElementById('app');
 const root = createRoot(container); 
-root.render(<App />);
+root.render(
+  <ChakraProvider>
+    <App />
+  </ChakraProvider>
+);
