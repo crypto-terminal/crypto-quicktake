b7ff98048d141948513dcf53a91067bf7edb9573
Yumin Gui <yumindev@gmail.com>
2022-05-23 22:39:07 -0700

    update script

--- postbuild.sh
+++ postbuild.sh
@@ -1,11 +1,4 @@
 #!/usr/bin/env bash
 
-INDEXHTML=dist/index.html
-if [[ -f "$INDEXHTML" ]]; then
-    mv dist/index.html dist/popup.html 
-fi
-
-MANIFEST=public/manifest.json
-if [[ -f "$MANIFEST" ]]; then
-   cp public/manifest.json dist/
-fi
+mv dist/index.html dist/popup.html 
+cp -r ./public/. dist
