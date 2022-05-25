e368c7f92b26483e57e6ed07aade0667dcef796d
Yumin Gui <yumindev@gmail.com>
2022-05-24 21:47:44 -0700

    update postbuild script

--- postbuild.sh
+++ postbuild.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 
-cp -r ./public/. dist
+./node_modules/.bin/terser --compress -- public/background.js > dist/background.js
+./node_modules/.bin/terser --compress -- public/content-script.js > dist/content-script.js
+cp public/manifest.json dist/
