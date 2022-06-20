122da96cd8eae19a48ff8ca6f63aeb532ffcebe2
Yumin Gui <yumindev@gmail.com>
2022-06-19 17:49:21 -0700

    add Add modal to Account Footer

--- src/components/Account/AccountFooter.jsx
+++ src/components/Account/AccountFooter.jsx
@@ -1,44 +1,136 @@
-import React from "react";
-import { useNavigate } from "react-router-dom";
-import { Button, Flex } from "@chakra-ui/react";
-import { FaUserPlus, FaFileExcel } from "react-icons/fa";
-
-const buttons = [
-  {
-    leftIcon: FaUserPlus,
-    text: "Add",
-    route: "/add",
-  },
-  {
-    leftIcon: FaFileExcel,
-    text: "Bulk Add",
-    route: "/bulk-add",
-  },
-];
+import React, { useCallback } from "react";
+import PropTypes from "prop-types";
+import {
+  Button,
+  Flex,
+  useDisclosure,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  FormControl,
+  FormLabel,
+  Input,
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+  HStack,
+} from "@chakra-ui/react";
+import { FaUserPlus, FaFileExcel, FaChevronDown } from "react-icons/fa";
+import { exchanges } from "../../constants";
 
 export const AccountFooter = () => {
-  const navigate = useNavigate();
+  const {
+    isOpen: isAddOpen,
+    onOpen: onAddOpen,
+    onClose: onAddClose,
+  } = useDisclosure();
+
+  const {
+    isOpen: isBulkAddOpen,
+    onOpen: onBulkAddOpen,
+    onClose: onBulkAddClose,
+  } = useDisclosure();
+
+  const handleAddOne = useCallback(() => {}, []);
   return (
-    <Flex
-      height="40px"
-      width="360px"
-      padding="0 10px"
-      justify="space-between"
-      alignItems="center"
-      boxShadow="base"
-    >
-      {buttons.map((button) => (
+    <React.Fragment>
+      <Flex
+        height="40px"
+        width="360px"
+        padding="0 10px"
+        justify="space-between"
+        alignItems="center"
+        boxShadow="base"
+      >
         <Button
-          key={button.text}
-          leftIcon={<button.leftIcon />}
+          leftIcon={<FaUserPlus />}
           colorScheme="blue"
           variant="outline"
           size="xs"
-          onClick={() => navigate(button.route)}
+          onClick={onAddOpen}
         >
-          {button.text}
+          Add
         </Button>
-      ))}
-    </Flex>
+        <Button
+          leftIcon={<FaFileExcel />}
+          colorScheme="blue"
+          variant="outline"
+          size="xs"
+          onClick={onBulkAddOpen}
+        >
+          Bulk Add
+        </Button>
+      </Flex>
+      <AddModal
+        isAddOpen={isAddOpen}
+        onAddClose={onAddClose}
+        handleAdd={handleAdd}
+      />
+    </React.Fragment>
   );
+};
+
+const AddModal = (props) => {
+  const { isAddOpen, onAddClose, handleAdd } = props;
+  return (
+    <Modal isOpen={isAddOpen} onClose={onAddClose}>
+      <ModalOverlay />
+      <ModalContent margin="auto 10px">
+        <ModalHeader>Add your API</ModalHeader>
+        <ModalCloseButton />
+        <ModalBody pb={6}>
+          <HStack spacing="8px">
+            <Menu>
+              <MenuButton
+                minWidth="unset"
+                as={Button}
+                rightIcon={<FaChevronDown />}
+              >
+                Select
+              </MenuButton>
+              <MenuList>
+                {exchanges.map((ex) => (
+                  <MenuItem key={ex.id} id={ex.id}>
+                    {ex.text}
+                  </MenuItem>
+                ))}
+              </MenuList>
+            </Menu>
+            <Input isReadOnly placeholder="Exchange or Wallet" size="md" />
+          </HStack>
+          <FormControl>
+            <FormLabel>API Key</FormLabel>
+            <Input placeholder="API Key" />
+          </FormControl>
+
+          <FormControl mt={4}>
+            <FormLabel>API secret</FormLabel>
+            <Input placeholder="API secret" />
+          </FormControl>
+        </ModalBody>
+
+        <ModalFooter>
+          <Button colorScheme="blue" mr={3} onClick={handleAdd}>
+            Save
+          </Button>
+          <Button onClick={onAddClose}>Cancel</Button>
+        </ModalFooter>
+      </ModalContent>
+    </Modal>
+  );
+};
+
+AddModal.propTypes = {
+  isAddOpen: PropTypes.func.isRequired,
+  onAddClose: PropTypes.func.isRequired,
+  handleAdd: PropTypes.func.isRequired,
+};
+
+const BulkAddModal = () => {
+  return <></>;
 };
