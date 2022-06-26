a9da4d38ee7607f2d91477550e10d053a8f298e8
Yumin Gui <yumindev@gmail.com>
2022-06-26 14:06:37 -0700

    operations to accounts

--- .eslintrc.js
+++ .eslintrc.js
@@ -25,6 +25,7 @@ module.exports = {
       }
     ],
     "react/react-in-jsx-scope": "off",
-    "react/jsx-fragments": "off"
+    "react/jsx-fragments": "off",
+    "react/jsx-filename-extension": [1, { extensions: [".js", ".jsx"] }]
   }
 };
--- src/components/Account/ApiKeySecretPairList.jsx
+++ src/components/Account/ApiKeySecretPairList.jsx
@@ -7,6 +7,7 @@ import {
   Tooltip,
   Text,
   HStack,
+  Badge
 } from "@chakra-ui/react";
 import { FaCog } from "react-icons/fa";
 
@@ -14,7 +15,7 @@ export const ApiKeySecretPairList = ({ pairs }) => {
   const isMainAccount = useCallback(() => {}, []);
 
   return (
-    <List spacing={3} width="100%" height="520px">
+    <List width="100%" height="520px">
       {pairs.map((pair) => (
         <ApiItem pair={pair} key={pair.apiKey} />
       ))}
@@ -40,16 +41,29 @@ const ApiItem = ({ pair }) => {
     const l = pair.apiKey.length;
     return `${pair.apiKey.slice(0, 6)}...${pair.apiKey.slice(l - 5)}`;
   }, []);
+
   return (
-    <ListItem width="100%">
+    <ListItem width="100%" mt={3}>
       <HStack spacing={1} width="100%">
         <ListIcon as={FaCog} color="green.500" />
-        <Text fontWeight={700}>{pair.ex.text}: &nbsp;</Text>
-        <Text maxWidth="120px" noOfLines={1}>
+        <Text fontWeight={700} minWidth="80px">
+          {pair.ex.text}:
+        </Text>
+        <Text w="80px" noOfLines={1}>
           <Tooltip hasArrow placement="top" label={pair.apiKey}>
-            {trucatedApiKey}
+            {trucatedApiKey /** fixed width */}
           </Tooltip>
         </Text>
+
+        <Badge variant="outline" colorScheme="green">
+          Main
+        </Badge>
+        <Badge variant="outline" colorScheme="green">
+          Edit
+        </Badge>
+        <Badge variant="outline" colorScheme="red">
+          Remove
+        </Badge>
       </HStack>
     </ListItem>
   );
